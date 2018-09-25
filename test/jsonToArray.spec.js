98fdbb33b83763c25302f29e27f4006d846a8cb4
Prateek <prateek.pronoc@gmail.com>
2018-09-25 15:14:56 +0530

    jsontoArray helper method issue #1

---
+++ test/jsonToArray.spec.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var expect = require('chai').expect;
+var helper = require('../index.js');
+describe('#IndexifyArray', function() {
+    it('should convert json to array object by attributes', function() {
+        var data = {
+            'id': 1,
+            'user': 'barney',
+            'active': false
+        };
+        var result = helper.jsonToArray(data);
+        expect(result).to.deep.equal([{
+                'id': 1
+            },
+            { 'user': 'barney' },
+            { 'active': false }
+        ]);
+    });
+});
