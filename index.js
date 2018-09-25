d1be2c915a16375c6634a9452381ced1be597a07
Prateek <prateek.pronoc@gmail.com>
2018-09-25 14:30:58 +0530

    added indexify method

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 'use strict';
 let _ = require('lodash');
 module.exports = {
-  addTwoNumber: (a, b) => {
-    return a + b;
-  },
-  /**
-   * Indexify the array
-   * @param {array} data
-   * @param {string} attribute
-   * @return {jsonObject}
-   */
-  indexifyArray: (data, attr) => {
-    return _.zipObject(_.map(data, attr), data);
-  }
+    addTwoNumber: (a, b) => {
+        return a + b;
+    },
+    /**
+     * Indexify the array
+     * @param {array} data
+     * @param {string} attribute
+     * @return {jsonObject}
+     */
+    indexifyArray: (data, attr) => {
+        return _.zipObject(_.map(data, attr), data);
+    },
+
+    /**
+     * Indexify the array of json object by properties of json object
+     * @param {array} data
+     * @param {string} attribute
+     * @param {string} valueAttribute
+     * @return {jsonObject}
+     */
+    indexify: (data, attr, valueAttr) => {
+        return _.zipObject(_.map(data, attr), _.map(data, valueAttr));
+    }
 };
---
+++ test/indexify.spec.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var expect = require('chai').expect;
+var helper = require('../index.js');
+describe('#IndexifyArray', function() {
+    it('should convert array to json object by attributes', function() {
+        var data = [{
+            'id': 1,
+            'user': 'barney',
+            'active': false
+        }, {
+            'id': 2,
+            'user': 'fred',
+            'active': false
+        }, {
+            'id': 3,
+            'user': 'pebbles',
+            'active': true
+        }];
+        var result = helper.indexify(data, 'id', 'user');
+        expect(result).to.deep.equal({
+            '1': 'barney',
+            '2': 'fred',
+            '3': 'pebbles'
+        });
+    });
+});
